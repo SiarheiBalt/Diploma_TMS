846aa78157a74ede5b18c27c2ee28ae51433abdc
SiarheiBalt <jh-11@yandex.by>
2021-07-17 18:44:21 +0300

    pages

--- src/components/Preloader/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import picture from "./../../img/preloaders/preloaderWhiteSmoke.png";
-
-export const Preloader = () => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <img style={{ margin: "25px" }} src={picture} />
-    </div>
-  );
-};
